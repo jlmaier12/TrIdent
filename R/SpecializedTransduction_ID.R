1f89a47bc28401ccf1fc52d715c449bd3357d048
jlmaier <jlmaier@ncsu.edu>
2023-12-30 21:17:45 -0500

    changes to variable names

--- R/SpecializedTransduction_ID.R
+++ R/SpecializedTransduction_ID.R
@@ -2,7 +2,7 @@
 #'
 #' Search contigs classified as prophage-like for potential specialized transduction. Returns a list with the first object containing a summary table and the second object containing a list of plots of prophage-like patterns with associated specialzied transduction search results. If the plot is green, it has been identified as having potential specialized transduction.
 #'
-#' @param phageread_dataset A table containing contig names, coverages averaged over 100bp windows, and contig positions associated with mapping VLP-fraction reads to whole-community contigs
+#' @param VLP_pileup A table containing contig names, coverages averaged over 100bp windows, and contig positions associated with mapping VLP-fraction reads to whole-community contigs
 #' @param transductionclassification Output from TrIdent_Classifier
 #' @param noreadcov How many bp of no read coverage are encountered before specialized transduction searching stops? Default is 500.
 #' @param spectranslength How many bp of read coverage are needed for specialized transduction to be considered? Default is 2000.
@@ -12,18 +12,18 @@
 #'
 #' @examples
 #' \dontrun{
-#' Spec_Transduction <- SpecializedTransduction_ID(VLP_fracreadcov, TrIdent_results)
+#' Spec_Transduction <- SpecializedTransduction_ID(VLP_pileup=VLP_fracreadcov, transductionclassifications=TrIdent_results)
 #' Summary_Table <- spec_transduction[[1]]
 #' NODE_8_plot <- spec_transduction[[2]]$NODE_8
 #'
-#' Spec_Transduction_NODE1 <- SpecializedTransduction_ID(VLP_fracreadcov, TrIdent_results, specificcontig="NODE_1", noreadcov=1000, spectranslength=4000)
+#' Spec_Transduction_NODE1 <- SpecializedTransduction_ID(VLP_pileup=VLP_fracreadcov, transductionclassifications=TrIdent_results, specificcontig="NODE_1", noreadcov=1000, spectranslength=4000)
 #' }
- SpecializedTransduction_ID <- function(phageread_dataset, transductionclassification, specificcontig, noreadcov=500, spectranslength=2000, cleanup=TRUE){
+ SpecializedTransduction_ID <- function(VLP_pileup, transductionclassification, specificcontig, noreadcov=500, spectranslength=2000, cleanup=TRUE){
   transductionclassificationpatterns <- transductionclassification[[3]]
   transductionclassificationsummary <- transductionclassification[[1]]
   windowsize <- transductionclassification[[5]]
   if (cleanup==TRUE){
-  phageread_dataset <- readcovdf_formatter(phageread_dataset)
+  VLP_pileup <- readcovdf_formatter(VLP_pileup)
   }
   if(missing(specificcontig)) {
     potential_spec_transduction <- 0
@@ -35,7 +35,7 @@
       classification <- transductionclassificationpatterns[[i]][[8]]
       if (classification != "Prophage-like") next
       ref_name <- transductionclassificationpatterns[[i]][[9]]
-      spec_trans_pred <- spec_transduction_search_and_plot(ref_name, phageread_dataset, transductionclassificationpatterns, transductionclassificationsummary, windowsize, i, noreadcov, spectranslength)
+      spec_trans_pred <- spec_transduction_search_and_plot(ref_name, VLP_pileup, transductionclassificationpatterns, transductionclassificationsummary, windowsize, i, noreadcov, spectranslength)
       if(spec_trans_pred[[1]][[2]] == "yes") {
         potential_spec_transduction <- potential_spec_transduction +1}
       specialized_transduction_summary[J,c(1:6)] <- spec_trans_pred[[1]]
@@ -51,7 +51,7 @@
     for (i in  seq(1, length(transductionclassificationpatterns), 1)) {
       ref_name <- transductionclassificationpatterns[[i]][[9]]
       if (ref_name == specificcontig) {
-        spec_trans_pred <- spec_transduction_search_and_plot(ref_name, phageread_dataset, transductionclassificationpatterns, transductionclassificationsummary, windowsize, i, noreadcov, spectranslength)
+        spec_trans_pred <- spec_transduction_search_and_plot(ref_name, VLP_pileup, transductionclassificationpatterns, transductionclassificationsummary, windowsize, i, noreadcov, spectranslength)
         return(spec_trans_pred[[2]])
       }
     }
