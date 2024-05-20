6c3ce1bef7d07eec08d224a0b1f365f47d7340ed
jordenrabasco <jrabasc@ncsu.edu>
2024-05-20 12:27:52 -0400

    test name update

--- tests/testthat/test-TrIdent_Classifier.R
+++ tests/testthat/test-TrIdent_Classifier.R
@@ -1,6 +1,6 @@
 context("Trident_classifer")
 
 test_that("default_Trident_classifer", {
-  temp_tri_results <- TrIdent_Classifier(VLP_pileup=VLPFraction_sampledata, WC_pileup=WholeCommunity_sampledata, windowsize=1000, minblocksize=10000, maxblocksize=Inf, cleanup=TRUE)
-  expect_equal(temp_tri_results , TrIdent_results)
+  temp_tri_results <- TrIdent_Classifier(VLP_pileup=VLPFraction_sampledata, WC_pileup=WholeCommunity_sampledata)
+  expect_equal(temp_tri_results , default_trident_results)
 })
